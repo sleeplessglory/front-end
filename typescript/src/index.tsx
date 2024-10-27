128c4bb31df25d7fe22b4bab300b62ebe1073a4c
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-10-27 22:51:55 +0300

    New exercises and improvements

---
+++ typescript/src/index.tsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './sass/style.scss';
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
